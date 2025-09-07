fde65f9980fb66cb1aefefbb86c7987c2deba0c3
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-09-07 21:10:23 +0600

    Update ldr_relay.ino

--- ldr_relay.ino
+++ ldr_relay.ino
@@ -1,18 +1,18 @@
 //this version of LDR control is done using a relay
-int thresholdHigh = 530;
-int thresholdLow = 490;
-//calibration can be done to set the the high and low value
+uint8_t threshold=0;
+unsigned int sum=0;
+for(int i=0;i<1000;i++){
+	sum+=analogRead(A0);
+	delay(10);
+}
+threshold=sum/1000;
 void setup() {
   pinMode(3, OUTPUT);
 }
 void loop() {
-	if (analogRead(A0) < thresholdLow) {
+	if (analogRead(A0) < threshold) {
   digitalWrite(3,1); 
 	} 
-	else if (analogRead(A0) > thresholdHigh) {
-  digitalWrite(3,0); 
-	}
-
   else{
     digitalWrite(3,0);
   }
