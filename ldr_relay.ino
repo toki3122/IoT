9bbdcd7ef465513cfecfd3879663999ca4973118
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-09-07 22:12:07 +0600

    Update ldr_relay.ino

--- ldr_relay.ino
+++ ldr_relay.ino
@@ -1,20 +1,19 @@
-//this version of LDR control is done using a relay
-uint8_t threshold=0;
-unsigned int sum=0;
-for(int i=0;i<1000;i++){
-	sum+=analogRead(A0);
-	delay(10);
-}
-threshold=sum/1000;
+uint32_t threshold=0;
+unsigned long sum=0;
 void setup() {
-  pinMode(3, OUTPUT);
+	pinMode(3, OUTPUT);
+		for(int i=0;i<1000;i++){
+		sum+=analogRead(A0);
+		delay(10);
+	}
+	threshold=sum/1000;
 }
 void loop() {
-	if (analogRead(A0) < threshold) {
+	if (analogRead(A0)<threshold) {
   digitalWrite(3,1); 
 	} 
   else{
     digitalWrite(3,0);
   }
-  delay(1000);
+  delay(200);
 }
