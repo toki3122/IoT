cf963162f1bbf310526249deb0aba4a6e0371b5d
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-09-09 00:05:23 +0600

    Update temp_hum.ino

--- temp_hum.ino
+++ temp_hum.ino
@@ -8,36 +8,30 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16,2);
+  lcd.init();
   lcd.backlight();
 }
 
 void loop() {
   Serial.println("=================================");
   Serial.println("Sample DHT11...");
-
   byte temperature = 0;
   byte humidity = 0;
-
   int err = dht11.read(&temperature, &humidity, NULL);
   if (err != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); 
     Serial.println(err);
-    delay(2000);   // Wait longer before retrying
+    delay(2000);
     return;
   }
-
-  // Print to LCD
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp: "); lcd.print((int)temperature); lcd.print(" C");
   lcd.setCursor(0, 1);
   lcd.print("Humid: "); lcd.print((int)humidity); lcd.print(" %");
-
-  // Debug to Serial
   Serial.print("Temp = "); Serial.print((int)temperature);
   Serial.print(" C, Humidity = "); Serial.print((int)humidity);
   Serial.println(" %");
-
-  delay(2000); // DHT11 needs at least 2s between reads
+  delay(2000);
 }
+
