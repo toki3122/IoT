0077543eefa8aa3cf65aef19e788883666a546d6
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-09-08 23:51:24 +0600

    Update temp_hum.ino

--- temp_hum.ino
+++ temp_hum.ino
@@ -1,30 +1,43 @@
 #include <SimpleDHT.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+
 int pinDHT11 = 2;
 SimpleDHT11 dht11(pinDHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.backlight();
 }
+
 void loop() {
   Serial.println("=================================");
   Serial.println("Sample DHT11...");
 
   byte temperature = 0;
   byte humidity = 0;
-  int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); Serial.println(err);
-    delay(1000);
+
+  int err = dht11.read(&temperature, &humidity, NULL);
+  if (err != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); 
+    Serial.println(err);
+    delay(2000);   // Wait longer before retrying
     return;
   }
-  lcd.begin(16,2);
-  lcd.backlight();
-  lcd.print("tempature  "); lcd.print((int)temperature);
-  lcd.setCursor(0, 1);
-  lcd.print("humidity  ");
-  lcd.print((int)humidity);
-  delay(1000);
-}
 
+  // Print to LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: "); lcd.print((int)temperature); lcd.print(" C");
+  lcd.setCursor(0, 1);
+  lcd.print("Humid: "); lcd.print((int)humidity); lcd.print(" %");
+
+  // Debug to Serial
+  Serial.print("Temp = "); Serial.print((int)temperature);
+  Serial.print(" C, Humidity = "); Serial.print((int)humidity);
+  Serial.println(" %");
+
+  delay(2000); // DHT11 needs at least 2s between reads
+}
