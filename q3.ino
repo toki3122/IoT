709eb875e977aa1ffb6732f9622e51ed64b4f0e1
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-12-29 23:56:55 +0600

    Implement LED control based on serial input
    Added input handling for LED control based on user input.

--- q3.ino
+++ q3.ino
@@ -1,9 +1,38 @@
+#define led0 4
+#define led1 5
 void setup() {
+pinMode(led0, OUTPUT);
+pinMode(led1, OUTPUT);
 Serial.begin(9600);
-int a;
+String s;
+delay(100);
 Serial.print("enter input: ");
 while(!Serial.available()){}
-a=Serial.parseInt();
-
+delay(100);
+s=Serial.readString();
+if(s.length()!=4){
+  Serial.println("INVALID");
+}
+else if(s=="0000"){
+  Serial.println("INVALID");
+}
+else{
+  if(s[0]=='1'){
+    digitalWrite(led1, HIGH);
+    digitalWrite(led0, HIGH);
+  }
+  else if(s[0]=='0' && s[1]=='1'){
+    digitalWrite(led1, HIGH);
+    digitalWrite(led0, LOW);
+  }
+  else if(s[0]=='0' && s[1]=='0' && s[2]=='1'){
+    digitalWrite(led1, LOW);
+    digitalWrite(led0, HIGH);
+  }
+  else if(s[0]=='0' && s[1]=='0' && s[2]=='0' && s[3]=='1'){
+    digitalWrite(led1, LOW);
+    digitalWrite(led0, LOW);
+  }
+}
 }
 void loop() {}
